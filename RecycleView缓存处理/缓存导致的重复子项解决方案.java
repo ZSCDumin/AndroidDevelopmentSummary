03bf33a5ecfe09bcd5d7e5f8429b708a9a5d593e
ZSCDumin <2712220318@qq.com>
2018-06-22 17:53:27 +0800

    更新代码：RcycleView缓存清除、常见Android开发类集合

---
+++ RecycleView缓存处理/缓存导致的重复子项解决方案.java
@@ -0,0 +1,2 @@
+1. 方案一：
+   viewHolder.setIsRecyclable(false); //强制设置为不适用缓存功能
---
+++ 安卓开发工具类/AndroidUtilCode.java
@@ -0,0 +1,9 @@
+1. 项目的Github地址：
+    https://github.com/Blankj/AndroidUtilCode
+
+2. 工具使用配置：
+	implementation 'com.blankj:utilcode:1.17.0'
+
+3.如何使用：
+    //init it in the function of onCreate in ur Application
+    Utils.init(application);
